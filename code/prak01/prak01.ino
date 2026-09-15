ea3bb1eb1718d5b2cc6845e2753d230df14e4e60
Adellia Rahmadani <@adeldely39.com>
2026-09-15 16:40:54 +0700

    praktik 02

--- code/prak01/prak01.ino
+++ code/prak01/prak01.ino
@@ -1,40 +1,17 @@
-const int buttonPin = 4;
 const int ledPin = 5;
-
-int ledState = 0;
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
-
-  digitalWrite(ledPin, LOW);
-
-  Serial.println("Sistem Sudah Siap! Sakelar Toggle Aktif.");
+  Serial.println("Praktikum 1 - Digital Output Dimulai!");
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == HIGH) {
-
-    delay(200);
-
-    if (digitalRead(buttonPin) == HIGH) {
-
-      ledState = !ledState;
-
-      digitalWrite(ledPin, ledState);
-
-      if (ledState == 1) {
-        Serial.println("Tombol Ditekan -> LED Menyala");
-      } else {
-        Serial.println("Tombol Ditekan -> LED Mati");
-      }
-
-      while (digitalRead(buttonPin) == HIGH) {
-      }
-
-
-    }
-  }
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED Menyala");
+  delay(1000);
+  
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED Mati");
+  delay(5000);
 }
